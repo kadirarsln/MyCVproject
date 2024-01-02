14c2649b05eb48840a086057fa7aa97cfd2c36e6
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2024-01-02 18:56:02 +0300

    Hakkımda Admin Page added.
    Hakkımda sayfası için admin panel oluşturuldu. Aynı zamanda kodları yazıldı.

---
+++ MyCVproject/Hakkımda.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyCVproject
+{
+    public partial class Hakkımda : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.TBL_HAKKIMDATableAdapter dbHakkımda = new DataSet1TableAdapters.TBL_HAKKIMDATableAdapter();
+
+            TextBox1.Text = dbHakkımda.HakkımdaListele()[0].AD;
+            TextBox2.Text = dbHakkımda.HakkımdaListele()[0].SOYAD;
+            TextBox3.Text = dbHakkımda.HakkımdaListele()[0].ADRES;
+            TextBox4.Text = dbHakkımda.HakkımdaListele()[0].MAIL;
+            TextBox5.Text = dbHakkımda.HakkımdaListele()[0].TELEFON;
+            TextBox6.Text = dbHakkımda.HakkımdaListele()[0].FOTOGRAFD;
+            TextBox7.Text = dbHakkımda.HakkımdaListele()[0].KISANOT;
+
+        }
+    }
+}
