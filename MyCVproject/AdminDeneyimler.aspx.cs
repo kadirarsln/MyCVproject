b91c1e4c891c3c16c9ed92c7f33da804b100128d
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2024-01-03 15:58:25 +0300

    AdminDeneyimler page added.
    Deneyimleri listeleme işlemi yapıldı.

---
+++ MyCVproject/AdminDeneyimler.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyCVproject
+{
+    public partial class AdminDeneyimler : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.TBL_DENEYIMLERIMTableAdapter dbDeneyimler = new DataSet1TableAdapters.TBL_DENEYIMLERIMTableAdapter();
+            Repeater1.DataSource = dbDeneyimler.DeneyimListe();
+            Repeater1.DataBind();
+        }
+    }
+}
--- MyCVproject/Hakkımda.aspx
+++ MyCVproject/Hakkımda.aspx
@@ -41,7 +41,7 @@
                 <asp:TextBox ID="TextBox7" runat="server" CssClass="form-control" Height="100px" TextMode="MultiLine"></asp:TextBox>
             </div>
             <br />
-            <asp:Button ID="Button1" runat="server" Text="Güncelle" CssClass="btn btn-success" />
+            <asp:Button ID="Button1" runat="server" Text="Güncelle" CssClass="btn btn-success" OnClick="Button1_Click" />
 
         </div>
 
