a2b9a8e59b55081ecd5f02713bbec692c395ffc8
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2024-01-08 16:29:18 +0300

    AdminDelete Pages added.

---
+++ MyCVproject/AdminHobiDelete.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyCVproject
+{
+    public partial class AdminHobiDelete : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt16(Request.QueryString["ID"]);
+
+            DataSet1TableAdapters.TBL_HOBILERIMTableAdapter dbHobilerDelete = new DataSet1TableAdapters.TBL_HOBILERIMTableAdapter();
+            dbHobilerDelete.HobiDelete(Convert.ToInt16(id));
+            Response.Redirect("AdminHobiler.Aspx");
+        }
+    }
+}
